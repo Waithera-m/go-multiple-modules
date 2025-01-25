49d536d2d89914931597a11a6fa2bbf18cb7bc98
Waithera <njihiamary11@gmail.com>
2025-01-25 19:52:55 +0300

    initial commit

---
+++ greetings.go
@@ -0,0 +1,17 @@
+package greetings
+
+import (
+	"fmt"
+	"errors"
+)
+
+// Hello returns a greeting for the named person.
+func Hello(name string) (string, error) {
+
+	if name == "" {
+		return "", errors.New("Please provide a name")
+	}
+    // Return a greeting that embeds the name in a message.
+    message := fmt.Sprintf("Hi, %v. Welcome!", name)
+    return message, nil
+}
